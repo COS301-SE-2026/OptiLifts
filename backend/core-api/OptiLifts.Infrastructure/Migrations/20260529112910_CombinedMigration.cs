236d1f8183ec3bbf131898b967f0f50b53bd82ea
EdwinKusel1 <u24670058@tuks.co.za>
2026-05-29 14:38:26 +0200

    feature: db migrations to support imageUrl

---
+++ backend/core-api/OptiLifts.Infrastructure/Migrations/20260529112910_CombinedMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OptiLifts.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CombinedMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "exercises",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "exercises");
+        }
+    }
+}

6d71a124997bb733a50cedd006eccc5fca5723ef
AlessandroParavano <alessandrodparavano@gmail.com>
2026-06-18 22:14:39 +0200

    feature: updated user table

--- backend/core-api/OptiLifts.Domain/Users/User.cs
+++ backend/core-api/OptiLifts.Domain/Users/User.cs
@@ -9,6 +9,18 @@ public class User
     public string Email { get; set; } = string.Empty;
     public string EmailHash { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
+    [Encrypted]
     public string DisplayName { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public string? RefreshTokenHash { get; set; }
+    public DateTime? RefreshTokenExpiry { get; set; }
+    public int Level { get; set; }
+    [Encrypted]
+    public string? Weight { get; set; }
+    [Encrypted]
+    public string? Height { get; set; }
+
+    public bool Metric { get; set; }
+    public bool LightTheme { get; set; }
 }
--- backend/core-api/OptiLifts.Infrastructure/Database/Configurations/UserConiguration.cs
+++ backend/core-api/OptiLifts.Infrastructure/Database/Configurations/UserConiguration.cs
@@ -16,8 +16,15 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
         builder.Property(u => u.Email).HasColumnName("email").IsRequired();
         builder.Property(u => u.EmailHash).HasColumnName("email_hash").IsRequired().HasMaxLength(64);
         builder.Property(u => u.PasswordHash).HasColumnName("password_hash").IsRequired();
-        builder.Property(u => u.DisplayName).HasColumnName("display_name").IsRequired().HasMaxLength(100);
+        builder.Property(u => u.DisplayName).HasColumnName("display_name").IsRequired();
         builder.Property(u => u.CreatedAt).HasColumnName("created_at").IsRequired();
+        builder.Property(u => u.RefreshTokenHash).HasColumnName("refresh_token_hash");
+        builder.Property(u => u.RefreshTokenExpiry).HasColumnName("refresh_token_expiry");
+        builder.Property(u => u.Level).HasColumnName("level").IsRequired();
+        builder.Property(u => u.Weight).HasColumnName("weight");
+        builder.Property(u => u.Height).HasColumnName("height");
+        builder.Property(u => u.Metric).HasColumnName("metric").IsRequired();
+        builder.Property(u => u.LightTheme).HasColumnName("light_theme").IsRequired();
 
         //creates a unique index on the email hash as it's deterministic
         builder.HasIndex(u => u.EmailHash).IsUnique();

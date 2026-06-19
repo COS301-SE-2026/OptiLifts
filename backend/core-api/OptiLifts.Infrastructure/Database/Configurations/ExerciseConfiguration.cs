baaea78ceff4db7065bef65c152ff8f0bd43f93c
AlessandroParavano <alessandrodparavano@gmail.com>
2026-06-19 09:16:19 +0200

    fix: linting

--- backend/core-api/OptiLifts.Infrastructure/Database/Configurations/ExerciseConfiguration.cs
+++ backend/core-api/OptiLifts.Infrastructure/Database/Configurations/ExerciseConfiguration.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using OptiLifts.Domain.Workouts;
 using OptiLifts.Domain.Users;
+using OptiLifts.Domain.Workouts;
 
 namespace OptiLifts.Infrastructure.Database.Configurations;
 

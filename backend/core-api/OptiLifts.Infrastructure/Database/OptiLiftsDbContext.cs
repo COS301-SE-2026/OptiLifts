3038e66328506774785064c80a793bc81dc8c062
AlessandroParavano <alessandrodparavano@gmail.com>
2026-06-19 08:25:22 +0200

    feature: created usermodel table

--- backend/core-api/OptiLifts.Infrastructure/Database/OptiLiftsDbContext.cs
+++ backend/core-api/OptiLifts.Infrastructure/Database/OptiLiftsDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microsoft.Extensions.Configuration;
 using OptiLifts.Domain.Common;
+using OptiLifts.Domain.Messaging;
 using OptiLifts.Domain.Users;
 using OptiLifts.Domain.Workouts;
 using OptiLifts.Infrastructure.Security;
@@ -37,6 +38,8 @@ public class OptiLiftsDbContext : DbContext
     public DbSet<SecMuscle> SecMuscles { get; set; }
     public DbSet<WorkoutExercise> WorkoutExercises { get; set; }
     public DbSet<ScheduledEntry> ScheduledEntries { get; set; }
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<UserModel> UserModels { get; set; }
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)

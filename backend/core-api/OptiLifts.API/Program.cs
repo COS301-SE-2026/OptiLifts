71c7e07c0ef60944007e236672102ef9930da94f
JordanNaidoo <u24664155@tuks.co.za>
2026-06-29 22:10:29 +0200

    fix:linting

--- backend/core-api/OptiLifts.API/Program.cs
+++ backend/core-api/OptiLifts.API/Program.cs
@@ -121,7 +121,7 @@ app.UseAuthorization(); //authorization middleware
 app.MapControllers();
 
 //basic health check endpoint, doesn't need a controller as just a simple get rq
-app.MapGet("/api/healthCheck", () => Results.Ok(new { status = "Healthy", timestamp = DateTime.UtcNow })); 
+app.MapGet("/api/healthCheck", () => Results.Ok(new { status = "Healthy", timestamp = DateTime.UtcNow }));
 await app.RunAsync();
 
 public partial class Program

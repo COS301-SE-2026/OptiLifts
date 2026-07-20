7131380a8ffa0fc74507e176f59b30f1b2a819d2
EdwinKusel1 <u24670058@tuks.co.za>
2026-07-20 18:01:35 +0200

    test: The workout-details page's end-to-end tests

---
+++ e2e/workout-details.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Workout Details Page', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/workouts/33333333-3333-3333-3333-333333333333');//push day A's workoutId
+        await page.waitForLoadState('networkidle');
+    });
+
+    test('User can view a workout\'s details', async ({ page }) => {
+        await expect (page.getByText('Push Day A')).toBeVisible();
+        await expect (page.getByText('Volume1,080 KG')).toBeVisible();
+        await expect (page.getByText('Sets2')).toBeVisible();
+
+        await expect (page.getByText('Barbell Bench Press')).toBeVisible();
+        await expect (page.getByRole('paragraph').filter({ hasText: 'Chest' })).toBeVisible();
+        await expect (page.getByText('Rest time: 1:30 min')).toBeVisible();
+        await expect (page.getByText('160 KG x 8 reps')).toBeVisible();
+        await expect (page.getByText('Back Squat')).toBeVisible();
+        
+        await expect (page.getByRole('paragraph').filter({ hasText: 'Quadriceps' })).toBeVisible();
+        await expect (page.getByText('Rest time: 2 min')).toBeVisible();
+        await expect (page.getByText('1120 KG x 5 reps')).toBeVisible();
+        await expect (page.getByText('MuscleSetsChest1Quadriceps1')).toBeVisible();
+    });
+});

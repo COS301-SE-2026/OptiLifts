12d4b75934dabe4fb9124264ee69963de775ff34
EdwinKusel1 <u24670058@tuks.co.za>
2026-07-20 19:28:58 +0200

    test: End-to-end tests for the workout-detail page

--- e2e/workout-log-detail.spec.ts
+++ e2e/workout-log-detail.spec.ts
@@ -2,25 +2,24 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Workout Detail Page', () => {
     test.beforeEach(async ({ page }) => {
-        await page.goto('/workouts/33333333-3333-3333-3333-333333333333');//push day A's workoutId
+        await page.goto('/workouts/33333333-3333-3333-3333-333333333333/logs/58597dd0-e02c-416c-a4b0-cba560f21045');//push day A's workoutID with it's workoutLogId
         await page.waitForLoadState('networkidle');
     });
 
-    test('User can view a workout\'s details', async ({ page }) => {
+    test('User can view a workout log\'s details', async ({ page }) => {
         await expect (page.getByText('Push Day A')).toBeVisible();
+        await expect (page.getByText('Duration55m')).toBeVisible();
         await expect (page.getByText('Volume1,080 KG')).toBeVisible();
         await expect (page.getByText('Sets2')).toBeVisible();
 
         await expect (page.getByText('Barbell Bench Press')).toBeVisible();
         await expect (page.getByRole('paragraph').filter({ hasText: 'Chest' })).toBeVisible();
-        await expect (page.getByText('Rest time: 1:30 min')).toBeVisible();
-        await expect (page.getByText('160 KG x 8 reps')).toBeVisible();
+        await expect (page.locator('div').filter({ hasText: /^160 KG x 8 reps @ 8 RPE$/ }).first()).toBeVisible();
+
         await expect (page.getByText('Back Squat')).toBeVisible();
-        
         await expect (page.getByRole('paragraph').filter({ hasText: 'Quadriceps' })).toBeVisible();
-        await expect (page.getByText('Rest time: 2 min')).toBeVisible();
-        await expect (page.getByText('1120 KG x 5 reps')).toBeVisible();
-        
+        await expect (page.locator('div').filter({ hasText: /^1120 KG x 5 reps @ 8\.5 RPE$/ }).first()).toBeVisible();
+
         await expect (page.getByText('MuscleSetsChest1Quadriceps1')).toBeVisible();
     });
 });

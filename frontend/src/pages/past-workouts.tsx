21942a0864cba8e38a8dde4c8d8ed084fa34dbbb
JordanNaidoo <u24664155@tuks.co.za>
2026-07-22 13:40:46 +0200

    fix: forgot to remove testing console log

--- frontend/src/pages/past-workouts.tsx
+++ frontend/src/pages/past-workouts.tsx
@@ -73,7 +73,6 @@ export default function PastWorkoutsPage() {
                         if (imgRes.ok) {
                             const imgDetails = await imgRes.json();
                             setExerciseImages(imgDetails)
-                            console.log('Exercise images fetched:', imgDetails);
                         }
                     }
                 }

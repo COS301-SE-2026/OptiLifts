b93b87a539c339bbdb13f8b8da9fa954c8aa67f4
AlexLange1st <u24587312@tuks.co.za>
2026-07-24 23:18:39 +0200

    fix: achived at and is current replaced with a link to the workout logs in the PR table

--- backend/core-api/OptiLifts.Domain/Workouts/ExercisePr.cs
+++ backend/core-api/OptiLifts.Domain/Workouts/ExercisePr.cs
@@ -11,10 +11,9 @@ public class ExercisePr
     public Guid Id { get; set; } = Guid.NewGuid();
     public Guid UserId { get; set; }
     public Guid ExerciseId { get; set; }
+    public Guid WorkoutLogSetId { get; set; }
     public ExercisePrType PrType { get; set; }
     public float PrValue { get; set; }
     public float AchievedWeight { get; set; }
     public int AchievedReps { get; set; }
-    public DateTime AchievedAt { get; set; } = DateTime.UtcNow;
-    public bool IsCurrent { get; set; } = true;
 }

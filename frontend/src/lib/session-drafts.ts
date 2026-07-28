276c584b6ea8e7e7b028d8b9ec7c6164ffc5dbd5
AlessandroParavano <alessandrodparavano@gmail.com>
2026-07-28 23:27:34 +0200

    feature: added function to retrieve the draft

--- frontend/src/lib/session-drafts.ts
+++ frontend/src/lib/session-drafts.ts
@@ -9,6 +9,30 @@ export function saveDraft<T>(workoutId: string, draft: T): void {
   }
 }
 
+// used for actual retrieving the draft
+export function getDraftFromStorage(): { workoutId: string; workoutName: string } | null {
+  for (let i = 0; i < localStorage.length; i++) {
+    const keyInStorage = localStorage.key(i)
+
+    if (!keyInStorage?.startsWith(PREFIX)) {
+      continue
+    }
+
+    try {
+      const data = JSON.parse(localStorage.getItem(keyInStorage) ?? '') as { workoutId?: string; workoutName?: string }
+
+      if (data.workoutId && data.workoutName) {
+        return { workoutId: data.workoutId, workoutName: data.workoutName }
+      }
+    } 
+    catch {
+      continue
+    }
+  }
+
+  return null
+}
+
 export function getDraft<T>(workoutId: string): T | null {
   try {
     const data = localStorage.getItem(PREFIX + workoutId)

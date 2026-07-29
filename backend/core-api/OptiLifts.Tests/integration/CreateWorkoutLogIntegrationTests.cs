d4c88394beb6038f23f7405ae8a3c0c7e3294ac4
AlessandroParavano <alessandrodparavano@gmail.com>
2026-07-29 23:41:58 +0200

    feature: second integration test

--- backend/core-api/OptiLifts.Tests/integration/CreateWorkoutLogIntegrationTests.cs
+++ backend/core-api/OptiLifts.Tests/integration/CreateWorkoutLogIntegrationTests.cs
@@ -52,4 +52,33 @@ public class CreateWorkoutLogIntegrationTests : IntegrationTestBase
         res.EntryId.Should().NotBeEmpty();
         res.AlreadyExisted.Should().BeFalse();
     }
+
+    [Fact]
+    public async Task SameLogIdTwice_ToldAlreadyExist()
+    {
+        var userId = await SeedUserAsync("finish-session-2@example.com");
+        var exerciseId = await SeedExerciseAsync("Squat");
+        Client.DefaultRequestHeaders.Add("Cookie", $"access_token={GenerateToken(userId)}");
+
+        var createWOBody = new CreateWorkoutRequest(null, "Leg Day", [], []);
+        var createWOresp = await Client.PostAsJsonAsync("/api/workouts", createWOBody);
+        var WO = await createWOresp.Content.ReadFromJsonAsync<CreateWorkoutResult>();
+
+        var logId = Guid.NewGuid();
+        var logBody = new CreateWorkoutLogReq(
+            logId, null, null, DateTime.UtcNow.AddMinutes(-30), DateTime.UtcNow,
+            [new CreateWorkoutLogExerciseReq(exerciseId, null, 1, 0,
+                [new CreateWorkoutLogSetReq(null, "Normal", 10, 60f, null, null, 60, 7f, 1, 0)])]);
+
+        var resp1 = await Client.PostAsJsonAsync($"/api/workouts/{WO!.WorkoutId}/logs", logBody);
+        resp1.StatusCode.Should().Be(HttpStatusCode.Created);
+        var res1 = await resp1.Content.ReadFromJsonAsync<CreateWorkoutLogRes>();
+
+        var resp2 = await Client.PostAsJsonAsync($"/api/workouts/{WO.WorkoutId}/logs", logBody);
+        resp2.StatusCode.Should().Be(HttpStatusCode.OK);
+        var res2 = await resp2.Content.ReadFromJsonAsync<CreateWorkoutLogRes>();
+
+        res2!.EntryId.Should().Be(res1!.EntryId);
+        res2.AlreadyExisted.Should().BeTrue();
+    }
 }

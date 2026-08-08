4ec3ed2192b7e8d842c6fff02991061a0b2a4c9e
AlexLange1st <u24587312@tuks.co.za>
2026-08-08 20:59:18 +0200

    updated db context

--- backend/core-api/OptiLifts.Infrastructure/Database/OptiLiftsDbContext.cs
+++ backend/core-api/OptiLifts.Infrastructure/Database/OptiLiftsDbContext.cs
@@ -35,6 +35,8 @@ public class OptiLiftsDbContext : DbContext
     public DbSet<WorkoutLogExercise> WorkoutLogExercises { get; set; }
     public DbSet<WorkoutSetLog> WorkoutLogSets { get; set; }
     public DbSet<ExercisePr> ExercisePrs { get; set; }
+    public DbSet<ExerciseEstimation> ExerciseEstimations { get; set; }
+    public DbSet<UserRepRange> UserRepRanges { get; set; }
     public DbSet<Muscle> Muscles { get; set; }
     public DbSet<SecMuscle> SecMuscles { get; set; }
     public DbSet<WorkoutExercise> WorkoutExercises { get; set; }

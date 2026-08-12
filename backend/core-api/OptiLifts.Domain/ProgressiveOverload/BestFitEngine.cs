9e63c46efbd7977b779e333e5aa1f149cd9e5576
JordanNaidoo <u24664155@tuks.co.za>
2026-08-12 21:53:32 +0200

    fix: linting

--- backend/core-api/OptiLifts.Domain/ProgressiveOverload/BestFitEngine.cs
+++ backend/core-api/OptiLifts.Domain/ProgressiveOverload/BestFitEngine.cs
@@ -16,12 +16,13 @@ public static class BestFitEngine
         var firstD = points.First().Date;
         int n = points.Count;
 
-        double sumX=0; 
-        double sumY=0;
-        double sumX2=0;
-        double sumXY=0;
+        double sumX = 0;
+        double sumY = 0;
+        double sumX2 = 0;
+        double sumXY = 0;
 
-        foreach (var point in points){
+        foreach (var point in points)
+        {
             double x = (point.Date - firstD).TotalDays;
             double y = point.Metric;
 
@@ -36,7 +37,7 @@ public static class BestFitEngine
         if (denominator == 0)
         {
             return (0, sumY / n);
-        }  
+        }
         double m = ((n * sumXY) - (sumX * sumY)) / denominator;
         double c = (sumY - (m * sumX)) / n;
         return (m, c);
@@ -45,25 +46,25 @@ public static class BestFitEngine
     public static bool PlateauCheck(List<PODataPoint> points)
     {
         var recentPoints = points.TakeLast(4).ToList();
-        
-        var (m,_) = GetBestFitLine(recentPoints);
-        return m <= 0.1; 
+
+        var (m, _) = GetBestFitLine(recentPoints);
+        return m <= 0.1;
     }
 
     public static double PredictNextVal(List<PODataPoint> points)
     {
         var (m, c) = GetBestFitLine(points);
 
-        double gapTotal = 0; 
-        for (int i =0; i< points.Count-1; i++)
+        double gapTotal = 0;
+        for (int i = 0; i < points.Count - 1; i++)
         {
-            gapTotal += (points[i+1].Date - points[i].Date).TotalDays;
+            gapTotal += (points[i + 1].Date - points[i].Date).TotalDays;
         }
-        int avgGap =(int) Math.Round( gapTotal / (points.Count - 1));
+        int avgGap = (int)Math.Round(gapTotal / (points.Count - 1));
 
         var lastP = points.Last();
         var firstP = points.First();
-        
+
         var nextDate = lastP.Date.AddDays(avgGap);
 
         double x = (nextDate - firstP.Date).TotalDays;

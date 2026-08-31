df233f376e2e91845a87c9c0a6f1febaec415f92
AlexLange1st <u24587312@tuks.co.za>
2026-08-31 09:57:05 +0200

    feature: best fit engine tests

---
+++ backend/core-api/OptiLifts.Tests/unit/ProgressiveOverload/BestFitEngineTests.cs
@@ -0,0 +1,101 @@
+using FluentAssertions;
+using OptiLifts.Domain.ProgressiveOverload;
+
+namespace OptiLifts.Tests.ProgressiveOverload;
+
+public class BestFitEngineTests
+{
+    [Fact]
+    public void GetBestFitLine_LinearData_ReturnsExpectedSlopeAndIntercept()
+    {
+        var start = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var points = new List<PODataPoint>
+        {
+            new(start, 10d),
+            new(start.AddDays(1), 12d),
+            new(start.AddDays(2), 14d)
+        };
+
+        var (slope, intercept) = BestFitEngine.GetBestFitLine(points);
+
+        slope.Should().BeApproximately(2d, 0.0001d);
+        intercept.Should().BeApproximately(10d, 0.0001d);
+    }
+
+    [Fact]
+    public void GetBestFitLine_AllDatesEqual_ReturnsAverageMetricAndZeroSlope()
+    {
+        var date = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var points = new List<PODataPoint>
+        {
+            new(date, 10d),
+            new(date, 20d),
+            new(date, 30d)
+        };
+
+        var (slope, intercept) = BestFitEngine.GetBestFitLine(points);
+
+        slope.Should().Be(0d);
+        intercept.Should().Be(20d);
+    }
+
+    [Fact]
+    public void PlateauCheck_FlatRecentTrend_ReturnsTrue()
+    {
+        var start = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var points = Enumerable.Range(0, 4)
+            .Select(day => new PODataPoint(start.AddDays(day), 100d))
+            .ToList();
+
+        var result = BestFitEngine.PlateauCheck(points);
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void PlateauCheck_GrowingRecentTrend_ReturnsFalse()
+    {
+        var start = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var points = Enumerable.Range(0, 4)
+            .Select(day => new PODataPoint(start.AddDays(day), 100d + day))
+            .ToList();
+
+        var result = BestFitEngine.PlateauCheck(points);
+
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void PredictNextVal_PredictionExceedsTenPercentCap_ReturnsCappedValue()
+    {
+        var start = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var points = new List<PODataPoint>
+        {
+            new(start, 100d),
+            new(start.AddDays(7), 120d),
+            new(start.AddDays(14), 140d),
+            new(start.AddDays(21), 160d)
+        };
+
+        var result = BestFitEngine.PredictNextVal(points);
+
+        result.Should().BeApproximately(110d, 0.0001d);
+    }
+
+    [Fact]
+    public void PredictNextVal_PredictionBelowCap_ReturnsRegressionPrediction()
+    {
+        var start = new DateTime(2026, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var points = new List<PODataPoint>
+        {
+            new(start, 100d),
+            new(start.AddDays(7), 101d),
+            new(start.AddDays(14), 102d),
+            new(start.AddDays(21), 103d)
+        };
+
+        var result = BestFitEngine.PredictNextVal(points);
+
+        result.Should().BeApproximately(101d, 0.0001d);
+    }
+}

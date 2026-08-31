259163d731d80da844486d6a8d8da5dcc66b4b8d
AlexLange1st <u24587312@tuks.co.za>
2026-08-31 09:57:33 +0200

    feature: E1rm calculator unit tests

---
+++ backend/core-api/OptiLifts.Tests/unit/ProgressiveOverload/E1RMCalculatorTests.cs
@@ -0,0 +1,98 @@
+using FluentAssertions;
+using OptiLifts.Domain.ProgressiveOverload;
+using OptiLifts.Domain.Workouts;
+
+namespace OptiLifts.Tests.ProgressiveOverload;
+
+public class E1RMCalculatorTests
+{
+    [Fact]
+    public void CalculateE1RM_BodyweightExercise_ReturnsReps()
+    {
+        var result = E1RMCalculator.CalculateE1RM(0f, 12, "compound", ExerciseType.BodyweightReps);
+
+        result.Should().Be(12);
+    }
+
+    [Fact]
+    public void ReverseEpleyReps_WeightedExercise_ReturnsRoundedRepTarget()
+    {
+        var result = E1RMCalculator.ReverseEpleyReps(100d, 75f, "compound", ExerciseType.WeightReps);
+
+        result.Should().Be(10);
+    }
+
+    [Fact]
+    public void ReverseEpleyReps_BodyweightExercise_ReturnsRoundedMetricAsReps()
+    {
+        var result = E1RMCalculator.ReverseEpleyReps(12.6d, 0f, null, ExerciseType.BodyweightReps);
+
+        result.Should().Be(13);
+    }
+
+    [Fact]
+    public void ReverseEpleyReps_WeightedExerciseWithNegativeCalculatedReps_ReturnsAtLeastOne()
+    {
+        var result = E1RMCalculator.ReverseEpleyReps(50d, 100f, null, ExerciseType.WeightReps);
+
+        result.Should().Be(1);
+    }
+
+    [Theory]
+    [InlineData(0d)]
+    [InlineData(-1d)]
+    public void ReverseEpleyReps_InvalidTargetE1RM_ThrowsArgumentOutOfRangeException(double targetE1RM)
+    {
+        var action = () => E1RMCalculator.ReverseEpleyReps(targetE1RM, 75f, null, ExerciseType.WeightReps);
+
+        action.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Theory]
+    [InlineData(0f)]
+    [InlineData(-5f)]
+    public void ReverseEpleyReps_InvalidPreviousWeight_ThrowsArgumentOutOfRangeException(float previousWeight)
+    {
+        var action = () => E1RMCalculator.ReverseEpleyReps(100d, previousWeight, null, ExerciseType.WeightReps);
+
+        action.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void ReverseEpleyWeight_WeightedExercise_ReturnsTargetWeight()
+    {
+        var result = E1RMCalculator.ReverseEpleyWeight(100d, 10, null, ExerciseType.WeightReps);
+
+        result.Should().BeApproximately(75.01875f, 0.001f);
+    }
+
+    [Fact]
+    public void ReverseEpleyWeight_AndReverseEpleyReps_AreInverseForSameRepTarget()
+    {
+        const double targetE1RM = 120d;
+        const int targetReps = 8;
+
+        var targetWeight = E1RMCalculator.ReverseEpleyWeight(targetE1RM, targetReps, null, ExerciseType.WeightReps);
+        var calculatedReps = E1RMCalculator.ReverseEpleyReps(targetE1RM, targetWeight, null, ExerciseType.WeightReps);
+
+        calculatedReps.Should().Be(targetReps);
+    }
+
+    [Fact]
+    public void ReverseEpleyWeight_BodyweightExercise_ThrowsInvalidOperationException()
+    {
+        var action = () => E1RMCalculator.ReverseEpleyWeight(100d, 8, null, ExerciseType.BodyweightReps);
+
+        action.Should().Throw<InvalidOperationException>();
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void ReverseEpleyWeight_InvalidTargetReps_ThrowsArgumentOutOfRangeException(int targetReps)
+    {
+        var action = () => E1RMCalculator.ReverseEpleyWeight(100d, targetReps, null, ExerciseType.WeightReps);
+
+        action.Should().Throw<ArgumentOutOfRangeException>();
+    }
+}

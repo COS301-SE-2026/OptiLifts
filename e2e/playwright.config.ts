6552f0b62c8d38bd85387c9593b3b3748bf8d9ab
EdwinKusel1 <u24670058@tuks.co.za>
2026-09-03 23:19:14 +0200

    fix: remove webkit

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -34,13 +34,7 @@ export default defineConfig({
             use: {
                 ...devices['Desktop Firefox'],
             },
-        },
-        {
-            name: 'webkit',
-            use: {
-                ...devices['Desktop Safari'],
-            },
-        },
+        }
     ],
 
     webServer: useExistingServices ? undefined : {

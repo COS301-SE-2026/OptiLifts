8439e37f1c055e0e0daa5ae501dcd68be89a0255
EdwinKusel1 <u24670058@tuks.co.za>
2026-09-13 19:44:20 +0200

    fix: e2e

--- e2e/create-workouts.spec.ts
+++ e2e/create-workouts.spec.ts
@@ -25,8 +25,6 @@ test.describe('Create Workouts Page', () => {
         await page.getByRole('textbox').nth(2).fill('100');
         await page.getByRole('textbox').nth(2).press('Tab');
         await page.getByRole('textbox').nth(3).fill('8');
-        await page.getByRole('button', { name: 'No rest' }).click();
-        await page.getByRole('menuitem', { name: '1:00' }).click();
         await page.getByRole('textbox', { name: 'Workout Name' }).click();
         await page.getByRole('textbox', { name: 'Workout Name' }).fill(uniqueWorkoutName);
 

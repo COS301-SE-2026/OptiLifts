62a963a35a6b017aba3ca1872eaac2c3b9714885
JordanNaidoo <u24664155@tuks.co.za>
2026-09-19 14:55:00 +0200

    squat model v1 - yipee

---
+++ optivision/training/train_squat.py
@@ -0,0 +1,108 @@
+from pathlib import Path
+
+import torch.nn.functional as F
+import torch
+from torch.utils.data import DataLoader, random_split
+
+from datasets.exercise_dataset import ExerciseDataset
+from architecture.cnn_1d import ExerciseCNN1D
+
+def pad_batch(batch):
+    max_frames = max([item[0].shape[1] for item in batch])
+        
+    padded_tensors = []
+    labels_list = []
+    
+    for tensor, labels in batch:
+        pad_size = max_frames - tensor.shape[1]
+        padded_tensor = F.pad(tensor, (0, pad_size))
+        
+        padded_tensors.append(padded_tensor)
+        labels_list.append(labels)
+        
+    batch_tensors = torch.stack(padded_tensors)
+    batch_labels = torch.stack(labels_list)
+    
+    return batch_tensors, batch_labels
+
+def main(): 
+    # params
+    TENSOR_DIR = Path("data/processed_tensors")
+    WEIGHTS_DIR = Path("weights")
+    WEIGHTS_DIR.mkdir(exist_ok=True)
+
+    EPOCHS = 50
+    BATCH_SIZE = 16
+    LEARNING_RATE = 0.001
+    NUM_CLASSES = 3
+
+    best_val_loss = float('inf')
+
+    device = torch.device("cuda")
+
+    # load dataset
+
+    full_dataset = ExerciseDataset(TENSOR_DIR, "squat", num_classes=NUM_CLASSES) 
+    train_size = int(0.8 * len(full_dataset))
+    val_size = len(full_dataset) - train_size
+    train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4, collate_fn=pad_batch)
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=4, collate_fn=pad_batch)
+
+    print(f"Training size: {train_size}, Validation size: {val_size}")
+
+    # model and optimizer
+
+    model = ExerciseCNN1D(num_classes=NUM_CLASSES).to(device)
+    criterion = torch.nn.BCELoss()
+    #  wow 314 stuff
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=1e-4)
+
+    # training loop
+    for epoch in range(EPOCHS):
+        model.train()
+        train_loss = 0.0
+
+        for tensors, labels in train_loader:
+            tensors, labels = tensors.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(tensors)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item() 
+
+        avg_train_loss = train_loss / len(train_loader)
+
+        model.eval()
+        val_loss = 0.0
+
+        with torch.no_grad():
+            for tensors, labels in val_loader:
+                tensors, labels = tensors.to(device), labels.to(device)
+
+                outputs = model(tensors)
+                loss = criterion(outputs, labels)
+
+                val_loss += loss.item()
+
+        avg_val_loss = val_loss / len(val_loader)
+
+        if (epoch + 1) % 10 == 0 or epoch == 0:
+                print(f"Epoch [{epoch+1}/{EPOCHS}] | Train Loss: {avg_train_loss:.4f} | Val Loss: {avg_val_loss:.4f}")
+
+
+        # save best model
+        if avg_val_loss < best_val_loss:
+            best_val_loss = avg_val_loss
+            
+            model = model.to("cpu")
+            scripted_model = torch.jit.script(model)
+            scripted_model.save(WEIGHTS_DIR / "squat_side.pt")
+            model = model.to(device) 
+
+if __name__ == "__main__":
+    main()

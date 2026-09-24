b0cfb671e1ce35ac39e6e3005c5b0e43d0498c00
AlexLange1st <u24587312@tuks.co.za>
2026-09-24 12:22:23 +0200

    feature: config for worker node

--- optivision/worker/worker.py
+++ optivision/worker/worker.py
@@ -17,3 +17,20 @@ import torch
 from azure.servicebus import ServiceBusClient, ServiceBusReceiver, ServiceBusReceivedMessage
 from azure.storage.blob import BlobClient
 from dotenv import load_dotenv
+
+load_dotenv()
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] [%(levelname)s] [AMQP Worker] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger("OptiVisionWorker")
+
+#config
+SERVICEBUS_CONN_STR = os.getenv("SERVICEBUS_CONNECTION_STRING")
+QUEUE_NAME = os.getenv("SERVICEBUS_QUEUE_NAME", "form-analysis-jobs")
+STORAGE_CONN_STR = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+CORE_API_URL = os.getenv("CORE_API_URL", "https://api.optilifts.app").rstrip("/")
+INTERNAL_SECRET = os.getenv("INTERNAL_WORKER_SECRET", "")
+HEARTBEAT_INTERVAL_SECONDS = int(os.getenv("HEARTBEAT_INTERVAL_SECONDS", "15"))
+APPLICATION_JSON = "application/json"

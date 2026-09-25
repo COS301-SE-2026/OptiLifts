13b931256bb5b18cf0c826dcf38a204258513bf8
JordanNaidoo <u24664155@tuks.co.za>
2026-09-25 14:59:36 +0200

    fix: better model stats

--- optivision/data_prep/augment_advanced.py
+++ optivision/data_prep/augment_advanced.py
@@ -13,7 +13,7 @@ EXERCISES = {
     },
     "deadlift": {
         "classes": 5,
-        "lacking_indices": [1],  # hips_early_rise
+        "lacking_indices": [1],  # hips early rise 
     },
 }
 
